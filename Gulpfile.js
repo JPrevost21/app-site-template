34423b3e77269a27c961cfb22a6369528642b635
Joseph <joeprevost21@gmail.com>
2017-07-18 13:22:25 -0400

    Lots of cosmetic changes

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,16 +1,26 @@
 let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('minify-css', () => {
   return gulp.src('src/**/*.css')
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist'));
 });
 
 var minify = require('gulp-minify');
- 
+const jshint = require('gulp-jshint');
+const stylish = require('jshint-stylish');
+
 gulp.task('compress', function() {
   return gulp.src('src/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish')) 
+    .pipe(jshint.reporter('fail')) 
     .pipe(minify({
         ext:{
             src:'-debug.js',
@@ -19,28 +29,17 @@ gulp.task('compress', function() {
         exclude: ['tasks'],
         ignoreFiles: ['.combo.js', '-min.js']
     }))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist')) 
 });
 
-gulp.task('copy-img', function () {
+gulp.task('copy-html', function () {
     gulp.src('src/*.html')
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('copy-html', function () {
+gulp.task('copy-img', function () {
     gulp.src('src/img/*.*')
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist/img'));
 });
 
-const autoprefixer = require('gulp-autoprefixer');
- 
-gulp.task('autoprefixer', () =>
-    gulp.src('src/**/*.css')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('dist'))
-);
-
-gulp.task('default', ['minify-css', 'compress', 'copy-img', 'copy-html', 'autoprefixer'],);
+gulp.task('default', ['minify-css', 'compress', 'copy-img', 'copy-html'],);
